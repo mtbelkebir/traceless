a991c270006e63756c4aa766d3d011f98ef49c39
mtbelkebir <mohandtahar.belkebir@se.univ-bejaia.dz>
2024-07-31 14:27:11 +0100

    Add Database context

---
+++ Traceless/Data/TracelessDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace Traceless.Data
+{
+    public class TracelessDbContext : IdentityDbContext
+    {
+    }
+}
--- Traceless/Program.cs
+++ Traceless/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using Traceless.Data;
+
 namespace Traceless
 {
     public class Program
@@ -13,6 +16,9 @@ namespace Traceless
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<TracelessDbContext>(options =>
+                options.UseInMemoryDatabase("TracelessDB"));
 
             var app = builder.Build();
 
